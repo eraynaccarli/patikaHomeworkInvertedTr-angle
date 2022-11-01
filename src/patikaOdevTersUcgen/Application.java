e1d1a1a5a9145a649127d10df9035f1c44f071a8
Eray Nacarlı <69572868+eraynaccarli@users.noreply.github.com>
2022-11-01 15:27:45 +0300

    Update Application.java

--- src/patikaOdevTersUcgen/Application.java
+++ src/patikaOdevTersUcgen/Application.java
@@ -10,10 +10,12 @@ public class Application {
 		System.out.print("Please enter number: ");
 		int number = sc.nextInt();
 		
+		// i 'yi sıfır'dan baslat girdigimiz sayiya kadar birbir arttır.
 		for(int i = 0; i < number; i++) {
-			for(int j = 0 ; j <= i; j++) {
-				System.out.print(" ");
+			for(int j = 0 ; j <= i; j++) { // j'yi sıfır'dan baslat i'ye kucuk esit olana kadar birbir arttır.
+				System.out.print(" "); // her arttırmada bosluk bas
 			}
+			// k'yi girdigimiz sayinin 2 katindan bir eksik olacak sekilde baslat ve i'nin iki katının bir fazlasına esit olana kadar tek tek azalt.
 			for(int k = 2 * number - 1;  k >= (2 * i + 1); k--) {
 				System.out.print("*");
 			}
